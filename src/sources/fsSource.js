37743c635f784b25d411f97011b52cdff1fddf9e
Noam Gal <atgardner@gmail.com>
2017-06-17 19:22:08 +0300

    added fsSource option

--- src/sources/fsSource.js
+++ src/sources/fsSource.js
@@ -1,6 +1,21 @@
+const { readFile } = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
+
+const readFileAsync = promisify(readFile);
+
 class FSSource {
   constructor(basePath) {
     this.basePath = basePath;
+  }
+
+  init() {
+    // do nothing
+  }
+
+  async getTileData(tile) {
+    const path = join(this.basePath, `${tile.zoom}`, `${tile.x}`, `${tile.y}.png`);
+    return readFileAsync(path);
   }
 }
 
