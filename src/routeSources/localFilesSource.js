1d948f3de12159fa8b9033b4aa9bcbc873f55452
Noam Gal <ATGardner@gmail.com>
2022-08-13 18:58:06 +0300

    updated dependencies
    Signed-off-by: Noam Gal <ATGardner@gmail.com>

--- src/routeSources/localFilesSource.js
+++ src/routeSources/localFilesSource.js
@@ -1,1 +1,67 @@
-import {readFileSync} from 'fs';import {extname} from 'path';import JSZip from 'jszip';import togeojson from '@mapbox/togeojson';import xmldom from 'xmldom';function readDocFromFile(fileName) {  const text = readFileSync(fileName, 'utf8');  return new xmldom.DOMParser().parseFromString(text);}async function readKmlStringFromKmz(fileName) {  const data = readFileSync(fileName);  const zip = new JSZip();  await zip.loadAsync(data);  const [docEntry] = zip.file(/\.kml$/);  return docEntry.async('text');}async function readFile(fileName) {  const ext = extname(fileName).toLocaleLowerCase();  switch (ext) {    case '.gpx': {      const doc = readDocFromFile(fileName);      return togeojson.gpx(doc);    }    case '.kml': {      const doc = readDocFromFile(fileName);      return togeojson.kml(doc);    }    case '.kmz': {      const kmlString = await readKmlStringFromKmz(fileName);      const doc = new xmldom.DOMParser().parseFromString(kmlString);      return togeojson.kml(doc);    }    default:      throw new Error('Unrecognized file type. Use only gpx/kml files.');  }}export default class LocalFilesSource {  constructor({inputFiles, routeAttribution}) {    this.inputFiles = inputFiles;    this.routeAttribution = routeAttribution;  }  get id() {    return this.inputFiles.join();  }  async getGeoJson() {    const promises = this.inputFiles.map(readFile);    const jsons = await Promise.all(promises);    const arrayOfFeatureArrays = jsons.map(j => j.features);    return {      features: Array.prototype.concat(...arrayOfFeatureArrays),      type: 'FeatureCollection',    };  }  toString() {    return this.inputFiles;  }}
+import { readFileSync } from 'fs';
+import { extname } from 'path';
+import JSZip from 'jszip';
+import { gpx, kml } from '@mapbox/togeojson';
+import { DOMParser } from '@xmldom/xmldom';
+
+function readDocFromFile(fileName) {
+  const text = readFileSync(fileName, 'utf8');
+  return new DOMParser().parseFromString(text);
+}
+
+async function readKmlStringFromKmz(fileName) {
+  const data = readFileSync(fileName);
+  const zip = new JSZip();
+  await zip.loadAsync(data);
+  const [docEntry] = zip.file(/\.kml$/);
+  return docEntry.async('text');
+}
+
+async function readFile(fileName) {
+  const ext = extname(fileName).toLocaleLowerCase();
+  switch (ext) {
+    case '.gpx': {
+      const doc = readDocFromFile(fileName);
+      return gpx(doc);
+    }
+
+    case '.kml': {
+      const doc = readDocFromFile(fileName);
+      return kml(doc);
+    }
+
+    case '.kmz': {
+      const kmlString = await readKmlStringFromKmz(fileName);
+      const doc = new DOMParser().parseFromString(kmlString);
+      return kml(doc);
+    }
+
+    default:
+      throw new Error('Unrecognized file type. Use only gpx/kml files.');
+  }
+}
+
+export default class LocalFilesSource {
+  constructor({ inputFiles, routeAttribution }) {
+    this.inputFiles = inputFiles;
+    this.routeAttribution = routeAttribution;
+  }
+
+  get id() {
+    return this.inputFiles.join();
+  }
+
+  async getGeoJson() {
+    const promises = this.inputFiles.map(readFile);
+    const jsons = await Promise.all(promises);
+    const arrayOfFeatureArrays = jsons.map(j => j.features);
+    return {
+      features: Array.prototype.concat(...arrayOfFeatureArrays),
+      type: 'FeatureCollection',
+    };
+  }
+
+  toString() {
+    return this.inputFiles;
+  }
+}
